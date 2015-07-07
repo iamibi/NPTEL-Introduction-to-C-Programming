a3b1b5c05a9dc209a160a9411c326563a9134dea
Syed Mohammad Ibrahim <sumiibi@gmail.com>
2015-07-07 15:03:35 +0530

    Calculate sum of adjacent pairs

--- SumOfAdjacentPairs.c
+++ SumOfAdjacentPairs.c
@@ -6,31 +6,29 @@
 
 int main()
 {
-    int current, sum = 0, prev = 0;
+    int current, sum = 0, prev;
 
+    // Take in the first values
+    scanf ("%d", &prev);
     scanf ("%d", &current);
 
-    // The first element is the current and the previous element.
-    prev = current;
-
-    // Keeping the record of sum.
-    sum = sum + prev;
-
-    while (1)
+    // Repeat until the current value is -1.
+    while (current != -1)
     {
-        // Taking more consecutive numbers.
+        // Calculate the Sum of previous and next
+        sum = prev + current;
+        printf ("%d", sum);
+
+        // Set previous to current.
+        prev = current;
+
+        // Again take in the input.
         scanf ("%d", &current);
 
-        // End if -1 is encountered.
         if (current == -1)
             break;
 
-        // Compute the sum by adding previous and the current number.
-        sum = prev + current;
-
-        // Set the previous number to current.
-        prev = current;
-        printf ("%d ", sum);
+        printf (" ");
     }
     printf ("\n");
     return 0;
